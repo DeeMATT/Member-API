c778a0d8586395c5b65161ef754eef6c02919c45
DeeMATT <mattode@outlook.com>
2019-08-29 13:47:09 +0100

    view function to handle put and patch request

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, g, request, jsonify
 from database import get_db
+import json
 
 app = Flask(__name__)
 
@@ -61,7 +62,31 @@ def add_member():
 
 @app.route('/member/<int:member_id>', methods=['PUT', 'PATCH'])
 def edit_member(member_id):
-    return 'This updates a member by ID.'
+    update_data = request.get_json()
+    db = get_db()    
+    if request.method == 'PATCH':
+        if 'name' in update_data.keys():
+            db.execute('UPDATE members SET name = ? WHERE id = ?' , [update_data['name'], member_id])
+            db.commit()
+        if 'email' in update_data.keys():
+            db.execute('UPDATE members SET email = ? WHERE id = ?' , [update_data['email'], member_id])
+            db.commit()
+        if 'level' in update_data.keys():
+            db.execute('UPDATE members SET level = ? WHERE id = ?' , [update_data['level'], member_id])
+            db.commit()
+
+    elif request.method == 'PUT':
+        name = update_data['name']
+        email = update_data['email']
+        level = update_data['level']
+
+        db.execute('UPDATE members SET name = ?, email = ?, level = ? WHERE id = ?', [name, email, level, member_id])
+        db.commit()
+
+    edited_cur = db.execute('SELECT * FROM members WHERE id = ?', [member_id])
+    member = edited_cur.fetchone()
+
+    return jsonify(member= {'id': member[0], 'name': member[1], 'email': member[2], 'level': member[3]})
 
 @app.route('/member/<int:member_id>', methods=['DELETE'])
 def delete_member(member_id):
